29966e78163ee15979d7952297c753a316ae76c9
John Doe <johndoe@example.com>
2017-11-10 14:35:56 -0700

    first commit

---
+++ highlow.rb
@@ -0,0 +1,24 @@
+#ask for numbers
+require "pry"
+puts "I can give you the high or low of numbers."
+puts "What are your numbers? Example = -1,7,2,87,32"
+
+#get numbers
+numbers = gets.chomp.to_i
+
+numbers =[]
+
+#turn numbers to int
+
+puts numbers
+binding.pry
+#get what they want high or low
+#run hi OR low
+
+def high
+  numbers.each do
+    
+  end
+end
+
+#RETURN number
